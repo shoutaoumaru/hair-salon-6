fdbe6cb6ce0cbd3e2ab2a626a98fc9fb27f15835
shoutaoumaru <otokmae.black@gmail.com>
2021-07-22 13:56:01 +0900

    モバイルメニュー予約ボタン

--- scripts/libs/svg-animation.js
+++ scripts/libs/svg-animation.js
@@ -1,9 +1,3 @@
-// new Vivus('move', {
-//   type: 'scenario-sync',
-//   duration: 200,
-//   forceRender: false,
-//   animTimingFunction: Vivus.EASE
-// })
 document.addEventListener('DOMContentLoaded', function () {
   new Vivus('move', {
     type: 'scenario-sync',
